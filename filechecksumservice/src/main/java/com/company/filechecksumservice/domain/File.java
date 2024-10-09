2e17203fd3b57a05b35b5154f7fbf21e2bfdfb2f
Malcinn <marcinkowalczyk1992@gmail.com>
2024-10-09 07:34:02 +0200

    Define Model

---
+++ filechecksumservice/src/main/java/com/company/filechecksumservice/domain/File.java
@@ -0,0 +1,13 @@
+package com.company.filechecksumservice.domain;
+
+public class File {
+
+    private Long id;
+
+    private String name;
+
+    private Long size;
+
+    private String checksum;
+    
+}
